da3943a08d934f975e37da95581aaf8b22345618
Achmad Rivaldi <mrtampan54@gmail.com>
2021-07-17 20:40:51 +0700

    updateee

--- README.md
+++ README.md
@@ -10,14 +10,16 @@
  
 ## Cara Menggunakan
 1.  jalankan
-git clone https://github.com/Cloud-Dark/pop-up-gift-card-happy-bhirtday
+git clone https://github.com/mrtampan/card-happybirthday.git
 2. Replace File img.jpeg sesuai file yang anda butuhkan
 3. Replace File music.mp3 Sesuai yang kalian inginkan
 4. Edit Text in Html
 5. Done
-    ![enter image description here](https://i.ibb.co/zG0RX3g/image.png)
-## Sepecial Thanks For
-- Image From [Pixels](https://www.pexels.com/search/couple%20in%20love/)
-- Music From [Youtube](https://www.youtube.com/watch?v=E5O6nXH9LeY)
-- Source From [Codepen](https://codepen.io/search/pens?q=birthday%20card)
-- And You
+
+## Jalankan dilocal server
+1. ketik perintah ini di cmd/terminal 
+``
+php -S 127.0.0.1:9090
+``
+2. buka di browser dengan url: 
+127.0.0.1:9090
--- script.js
+++ script.js
@@ -3,12 +3,25 @@
     return document.getElementById(id);
   }
 
+  
   var card = $("card"),
     openB = $("open"),
     closeB = $("close"),
+    txtHbd = $("textHbd"),
+    myAudio = new Audio
     timer = null;
+
+    myAudio.src = "music.mp3";
   console.log("wat", card);
   openB.addEventListener("click", function() {
+
+    if(myAudio.paused){
+      myAudio.play();
+    }else{
+      myAudio.pause();
+    }
+    document.title = 'Happy Birthday Mina';
+    txtHbd.setAttribute("class", "d-none");
     card.setAttribute("class", "open-half");
     if (timer) clearTimeout(timer);
     timer = setTimeout(function() {
@@ -18,6 +31,7 @@
   });
 
   closeB.addEventListener("click", function() {
+    txtHbd.setAttribute("class", "animated infinite swing")
     card.setAttribute("class", "close-half");
     if (timer) clearTimerout(timer);
     timer = setTimeout(function() {
@@ -25,4 +39,5 @@
       timer = null;
     }, 1000);
   });
+
 })();
